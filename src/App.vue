7d039c09d3046573e1560cad4c4f3a17e530fdd6
rexmyl <rexmyl09@gmail.com>
2024-08-13 17:26:30 +0800

    add items

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <script setup lang="ts">
+import SearchBox from './components/SearchBox.vue';
 
 </script>
 
 <template>
-  <header></header>
+  <header id="nav">
+    <div id="nav-left">
+      <img>
+      <div>
+
+      </div>
+    </div>  
+    <div id="nav-mid">
+      <div class="nav-word">
+        <a>首页</a>
+      </div>
+      <div class="nav-word">
+        <a>分类</a>
+      </div>
+      <div class="nav-word">
+        <a>关于</a>
+      </div>
+      <div class="nav-word">
+        <a>订阅</a>
+      </div>
+    </div>
+    <div id="nav-right">
+      <SearchBox></SearchBox>
+    </div>
+  </header>
   <main>
       
   </main>
   <footer>
-
+    <div>Copyright © 2024 Re.Blog</div> 
+    <div>QQ:841189483  Email:841189483@qq.com</div>
   </footer>
 </template>
 
--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -2,13 +2,35 @@
   
 </script>
 <template>
-<div>
-    <a>
-        <img />
+<div id="search-box">
+    <a id="search-ic">
+        <img src="/search.svg" alt="搜索：" style="width: 25px;height:25px;"/>
     </a>
-    <input/>
+    <input id="search-ct"/>
 </div>
 </template>
 
-<style>
+<style scoped>
+    #search-box{
+        position:relative;
+        display: flex;
+        flex-direction: row;
+        border: rgb(0,0,0,0) solid 2px;
+        border-radius:7px;
+        width:300px;
+        height:35px;
+    }
+
+    #search-ic img{
+        margin: 5px 5px;
+    }
+
+    #search-ct{
+        border: none;
+        width:85%;
+        outline: none;
+        line-height:35px ;
+        font-size: 18px;
+        text-align: left;
+    }
 </style>
